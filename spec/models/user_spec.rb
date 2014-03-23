2952b8819cfa0d1ae9dc4ba4daf41ba5d7c1ab8f
Philip Ingram <neodestro734@yahoo.com>
2014-03-23 14:50:17 -0700

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,8 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -108,26 +110,6 @@ describe User do
   end
 
   describe "return value of authenticate method" do
-  before { @user.save }
-  let(:found_user) { User.find_by(email: @user.email) }
-
-  describe "with valid password" do
-    it { should eq found_user.authenticate(@user.password) }
-  end
-
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
-  end
-
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-
-  describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by(email: @user.email) }
 
@@ -141,15 +123,69 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+
+    describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+    end
+
+    describe "return value of authenticate method" do
+      before { @user.save }
+      let(:found_user) { User.find_by(email: @user.email) }
+
+      describe "with valid password" do
+        it { should eq found_user.authenticate(@user.password) }
+      end
+
+      describe "with invalid password" do
+        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+        it { should_not eq user_for_invalid_password }
+        specify { expect(user_for_invalid_password).to be_false }
+      end
+    end
+
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
   end
 
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }
+  describe "micropost associations" do
 
-  describe "remember token" do
     before { @user.save }
-    its(:remember_token) { should_not be_blank }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
   end
-end
 end
